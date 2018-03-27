fa9051c25c3788fc89f58aac4aadbad29bcc8da9
alex-wenzel <atwenzel@eng.ucsd.edu>
2018-03-26 18:02:14 -0700

    some unit testing in R for DiffEx and L1KGCT

--- testing/evaluation_scripts/DiffEx_evaluation.R
+++ testing/evaluation_scripts/DiffEx_evaluation.R
@@ -2,15 +2,13 @@
 ### testing/res.df.txt. This plot should show increasing log2 fold change
 
 diffex.unit.upreg = read.csv("DiffEx_unit_upreg.tsv", sep='\t')
-x.axis.vals = seq(1, nrow(diffex.unit.upreg))
-plot(x.axis.vals, diffex.unit.upreg$log2FoldChange)
+plot(diffex.unit.upreg$log2FoldChange)
 
 ### This produces a plot of the sorted downregulated genes from
 ### testing/res.df.txt. This plot should show decreasing log2 fold change
 
 diffex.unit.dnreg = read.csv("DiffEx_unit_dnreg.tsv", sep='\t')
-x.axis.vals = seq(1, nrow(diffex.unit.dnreg))
-plot(x.axis.vals, diffex.unit.dnreg$log2FoldChange)
+plot(diffex.unit.dnreg$log2FoldChange)
 
 ### This code produces a histogram of expression values and expression z-scores
 
@@ -19,3 +17,22 @@ hist(l1k.unit$LINCSCP_1, breaks=200)
 hist(l1k.unit$LINCSCP_1_zscore, breaks=200)
 hist(l1k.unit$LINCSCP_1_zscore_norm, breaks=200)
 hist(l1k.unit$LINCSCP_1_zscore_norm_pos, breaks=200)
+
+### This code checks the sorting for the LINCS profile, this should plot
+### genes by increasing differential expression
+
+plot(l1k.unit$LINCSCP_1)
+
+### DiffEx+profile_upreg.tsv should contain the phenotype expression
+### joined with the drug profile, sorted according to ascending abs(diff_ex)
+
+diffex.up.join.prof = read.csv("DiffEx+profile_upreg.tsv", sep='\t')
+diffex.dn.join.prof = read.csv("DiffEx+profile_dnreg.tsv", sep='\t')
+
+### This code gets the gene symbols that are present l1k.unit
+### but not in diffex.up.join.prof or diffex.dn.join.prof
+
+all.gene.sym.l1k = as.character(l1k.unit$Name_GeneSymbol)
+all.gene.sym.up.dn = c(as.character(diffex.up.join.prof$Name_GeneSymbol),
+                       as.character(diffex.dn.join.prof$Name_GeneSymbol))
+setdiff(all.gene.sym.l1k, all.gene.sym.up.dn)
