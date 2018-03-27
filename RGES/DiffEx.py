05871e0f5275faa145c3a9675bdd7b82ed1607f8
alex-wenzel <atwenzel@eng.ucsd.edu>
2018-03-26 18:01:03 -0700

    implemented joining with drug profle

--- RGES/DiffEx.py
+++ RGES/DiffEx.py
@@ -13,6 +13,8 @@ table is required to have the following headers:
 
 import pandas as pd
 
+from L1KGCT import L1KGCT
+
 class DiffEx:
     """
     Represents a file with differential expression data as described above
@@ -27,6 +29,8 @@ class DiffEx:
             returns: None
         """
         self.data = pd.read_csv(path, sep=sep)
+        self.data['entrezgene'] = self.data['entrezgene'].astype(str)
+        self.data['entrezgene'] = self.data['entrezgene'].apply(lambda x: x[:-2])
 
     """
     Accession
@@ -40,7 +44,7 @@ class DiffEx:
         """
         up_gene_rows = self.data.ix[self.data['log2FoldChange']>0]
         ug_rows_sorted = up_gene_rows.sort_values(by=['log2FoldChange'])
-        return ug_rows_sorted[['gene_id', 'log2FoldChange']]  #return id because some symbols missing
+        return ug_rows_sorted[['entrezgene', 'log2FoldChange']]  #return id because some symbols missing
 
     def get_down_genes(self):
         """
@@ -49,15 +53,40 @@ class DiffEx:
             returns (pd.DataFrame): Downregulated genes in self.data
         """
         dn_gene_rows = self.data.ix[self.data['log2FoldChange']<0]
-        ##NOTE unclear if downregulated should be ascending or descending
         dg_rows_sorted = dn_gene_rows.sort_values(by=['log2FoldChange'], ascending=False)
-        return dg_rows_sorted[['gene_id', 'log2FoldChange']]  #return id because some symbols missing
+        return dg_rows_sorted[['entrezgene', 'log2FoldChange']]  #return id because some symbols missing
+
+    def get_profile_order(self, l1k_prof):
+        """
+        Uses an L1000 drug profile to re-order the upregulated and downregulated genes
+
+            l1k_prof (L1KGCT): A drug signature
+
+            returns ((pd.DataFrame, pd.DataFrame)): 
+        """
+        cols2save = ['entrezgene', 'log2FoldChange', 'Name_GeneSymbol', l1k_prof.name, 'drug_rank']
+
+        up = self.get_up_genes()
+        dn = self.get_down_genes()
+        up_prof = pd.merge(up, l1k_prof.data, how='left', left_on='entrezgene', right_on='ID_geneid')
+        up_prof = up_prof[up_prof['drug_rank'].notnull()][cols2save]
+
+        dn_prof = pd.merge(dn, l1k_prof.data, how='left', left_on='entrezgene', right_on='ID_geneid')
+        dn_prof = dn_prof[dn_prof['drug_rank'].notnull()][cols2save]
+
+        return up_prof, dn_prof
 
 if __name__ == "__main__":
     DIR = "/scratch/alexw/l1k/"
 
-    de = DiffEx("testing/res.df.txt")
+    de = DiffEx("testing/res.df.entrez.txt")
+    
     up = de.get_up_genes()
     dn = de.get_down_genes()
-    up.to_csv(DIR+"DiffEx_unit_upreg.tsv", sep='\t')
-    dn.to_csv(DIR+"DiffEx_unit_dnreg.tsv", sep='\t')
+    up.to_csv(DIR+"DiffEx_unit_upreg.tsv", sep='\t', index=False, na_rep="NA")
+    dn.to_csv(DIR+"DiffEx_unit_dnreg.tsv", sep='\t', index=False, na_rep="NA")
+
+    l = L1KGCT("testing/LINCSCP_1.gct", normalized=True)
+    up_prof, dn_prof = de.get_profile_order(l)
+    up_prof.to_csv(DIR+"DiffEx+profile_upreg.tsv", sep='\t', index=False, na_rep="NA")
+    dn_prof.to_csv(DIR+"DiffEx+profile_dnreg.tsv", sep='\t', index=False, na_rep="NA")
