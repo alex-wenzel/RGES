5fbbd4599e2b47a5a689583537a1ab259b32a37c
alex-wenzel <atwenzel@eng.ucsd.edu>
2018-04-02 15:46:19 -0700

    added a .gctx parser - waiting for cmapPy bugs to be resolved

--- RGES/L1KGCT.py
+++ RGES/L1KGCT.py
@@ -5,7 +5,12 @@ as provided by the iLINCS browser, which can be downloaded here:
 http://www.ilincs.org/ilincs/signaturesL1000/LDG-1188/search/
 """
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import cmapPy.pandasGEXpress as GEX
+import cmapPy.pandasGEXpress.write_gctx as write_gctx
 import pandas as pd
+import time
 
 class MultiL1KGCT:
     """
@@ -30,9 +35,10 @@ class MultiL1KGCT:
         self.path = path
         self.normalized = normalized
 
-        self.load()
-        if self.normalized:
-            self.normalize()
+        if path != None:
+            self.load()
+            if self.normalized:
+                self.normalize()
 
     """
     Loading Data
@@ -83,8 +89,88 @@ class MultiL1KGCT:
         col_d = {c: norm if c in self.metadata.keys() else no_change for c in list(self.data)}
         self.data = self.data.transform(col_d)
 
+class L1KGCTX(MultiL1KGCT):
+    """
+    This class overrides the loading methods for MultiL1KGCT to load data from
+    a gctx binary file using the cmapPy parsers
+
+    WARNING: DO NOT USE THIS CLASS UNTIL CMAPPY WORKS IN PYTHON 3
+
+    see https://github.com/cmap/cmapPy/issues/25
+    """
+    def __init__(self, path, normalized=False, sort=False):
+        """
+        Initializes an L1KGCTX instance - calls cmapPy parsers
+
+            path (str): A path to a .gctx file
+            normalized (bool): If True, apply normaliziation as described
+                                in self.normalize()
+            sort (bool): If True, sort and label each column of the dataframe
+                            by appending a new column with ranks for each
+
+            returns: None
+        """
+        super().__init__(None, normalized=normalized)
+
+        self.path = path
+        self.sort = sort
+
+        if self.path != None:
+            self.load()
+            if self.sort:
+                self.sort_label()
+            #if self.normalized:
+            #    self.normalize()
+
+    """
+    Loading Data
+    """
+
+    def load(self):
+        """
+        Calls the cmapPy gctx parser, retrieves matrix and metadata
+
+            returns: None
+        """
+        self.data = GEX.parse(self.path).data_df
+
+    """
+    Sort and Label
+    """
+
+    def sort_label(self):
+        """
+        Applies the pd.DataFrame.rank() function to each column individually
+        across all signatures
+
+            returns: None
+        """
+        for i, sig in enumerate(list(self.data)):
+            if i > 100:
+                break
+            self.data[sig+'_drug_rank'] = self.data[sig].rank()
+
+    """
+    Save
+    """
+
+    def save(self, path):
+        """
+        Saves a .gctx file from self.data to path
+        """
+        gctoo = GEX.GCToo.GCToo(self.data)
+        write_gctx.write(gctoo, path)
+
 if __name__ == "__main__":
     DIR = "/scratch/alexw/l1k/"    
 
-    ml = MultiL1KGCT("testing/CTPRES_100_concordant_sigs.gct", normalized=True)
-    ml.data.to_csv(DIR+"mul1k_unit_zscore.tsv", sep='\t', index=False, na_rep="NA")
+    #ml = MultiL1KGCT("testing/CTPRES_100_concordant_sigs.gct", normalized=True)
+    #ml.data.to_csv(DIR+"mul1k_unit_zscore.tsv", sep='\t', index=False, na_rep="NA")
+
+    #lincs_rel_path = "/scratch/alexw/l1k/LINCS_FULL_GEO/GSE70138_2017-03-06_l5_landmarks.gctx_n118050x972.gctx"
+    #gctx = L1KGCTX(lincs_rel_path, normalized=True, sort=False)
+    #gctx.save("/scratch/alexw/l1k/LINCS_FULL_GEO/GSE70138_ranked.gctx")
+
+    #lincs_path = "/scratch/alexw/l1k/LINCS_FULL_GEO/GSE70138_Broad_LINCS_Level5_COMPZ_n118050x12328_2017-03-06.gctx"
+    #gctoo_1 = GEX.parse(lincs_rel_path)
+    #write_gctx.write(gctoo_1, "test.gctx")
